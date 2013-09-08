26566d92b88b2f39ad1faca526892c46656c6d2f
Lauren Reeder <laurenmreeder@gmail.com>
2013-09-08 00:02:45 -0400

    renamed test.py, also minor edits to pull_from_canvas.py

--- pull_from_canvas.py
+++ pull_from_canvas.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from canvas2dropbox import selenium_test
 import time
 import sys
 import requests
@@ -15,9 +16,6 @@ reqName=requests.get(urlCourse, params=params)
 courseInfo=reqName.json()
 courseName=courseInfo['name']
 
-r=requests.get(url, params=params)
-rootInfo=r.json()
-loop_through_folders("/{}/".format(courseName),"", rootInfo)
 #first call of this should have location be root folder "/", name is the courseName, folder is rootInfo
 def loop_through_folders(location, name, folder):
 	if folder['files_count']>0:
@@ -27,10 +25,19 @@ def loop_through_folders(location, name, folder):
 			fileAddress=item['url']
 			fileName=item['display_name']
 			test.download(fileAddress)
-			subprocess.call("find_most_recent.sh",shell=True) #parameters for new folder creation, etc
+			subprocess.call("find_most_recent.sh",shell=True)
+
 	if folder['folders_count']>0:
 		reqFolders=requests.get(folder['folders_url'], params=params)
 		foldersInfo=reqFolders.json()
 		for item in foldersInfo:
 			loop_through_folders("{}/{}/".format(location, item['name']), item['name'], item)
+	return
+
+
+r=requests.get(url, params=params)
+rootInfo=r.json()
+loop_through_folders("/{}/".format(courseName),"", rootInfo)
+
+
 
